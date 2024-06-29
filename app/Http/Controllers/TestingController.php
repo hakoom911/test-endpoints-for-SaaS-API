cf8b344f3baf4429f48c66da6011ff81a2f8313b
hakoom911 <114762298+hakoom911@users.noreply.github.com>
2024-06-29 03:04:11 +0300

    adding the api files and it deps and right the controller first test function

--- app/Http/Controllers/TestingController.php
+++ app/Http/Controllers/TestingController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class TestingController extends Controller
 {
     //
+    public function postFile(Request $request){
+
+        if($request->hasFile('file')){
+        $file=Storage::disk('local')->put($request->file, 'Contents');
+        $path = Storage::url($file);
+            if($file){
+                return response()->json('file stored successfully: '.$path,200);
+            }else{
+                return response()->json('fuck you ',400);
+            }
+
+        }
+
+    }
+
+
 }
